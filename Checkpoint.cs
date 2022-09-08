bf453347460fe91d213ffbe66f3e2b6172f6d60c
HajarOuq <77498141+HajarOuq@users.noreply.github.com>
2022-09-08 13:57:04 +0100

    Add files via upload

---
+++ Checkpoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Checkpoint
+{
+	public Checkpoint()
+	{
+	}
+
+    float sum(float a, float b)
+    {
+        return (a + b);
+    }
+
+    float prod(float a, float b)
+    {
+        return (a * b);
+    }
+
+    float div(float a, float b)
+    {
+        if (b == 0)
+        {
+            Console.WriteLine("Error");
+            return 0;
+        }
+        else
+        {
+            return (a / b);
+        }
+    }
+}
